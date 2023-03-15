d8fe62c8c8dc0f28813fb12713326089ca51d38c
madhanmohans <madhanmohan.rms@gmail.com>
2023-03-15 15:03:22 +0530

    updated spinner screen ui

--- app/src/main/java/com/example/fnvMockJetpack/components/SpinnerScreen.kt
+++ app/src/main/java/com/example/fnvMockJetpack/components/SpinnerScreen.kt
@@ -1,20 +1,18 @@
 package com.example.fnvMockJetpack.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.DropdownMenu
 import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.fnvMockJetpack.components.ui.theme.FnvMockJetpackTheme
@@ -27,38 +25,55 @@ fun SpinnerScreen(
     onItemSelected: (selectedItem : String) -> Unit
 )
 {
-    var tempSelectedItem=selectedItem;
+    var tempSelectedItem = selectedItem
+
     if (tempSelectedItem.isBlank() && spinnerList.isNotEmpty())
     {
         onItemSelected(spinnerList[0])
         tempSelectedItem=spinnerList[0]
     }
-    var expanded by rememberSaveable() { mutableStateOf(false) }
 
-    OutlinedButton(onClick = { expanded=true },
-        modifier = Modifier
-            .background(Color.White)
-            .widthIn(min = 60.dp),
-        enabled=spinnerList.isNotEmpty() ) {
-        Text(text = spinnerName,
-            overflow = TextOverflow.Ellipsis,
-            maxLines=1,
-            modifier = Modifier.weight(1f,
-            ),)
-        Text(text = tempSelectedItem,
-            overflow = TextOverflow.Ellipsis,
-            maxLines=1,
-            fontWeight = FontWeight.SemiBold,
-            modifier = Modifier.weight(.5f,
-            ),)
-    }
-    DropdownMenu(expanded = expanded,
-        onDismissRequest = { expanded=false}) {
-        spinnerList.forEach{
-            DropdownMenuItem(onClick = { expanded=false
-                onItemSelected(it)
-            }) {
-                Text(text = it)
+    var expanded by remember { mutableStateOf(false) }
+
+    Row(
+        modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            modifier = Modifier.weight(1f),
+            text = spinnerName,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold
+        )
+
+        OutlinedButton(
+            onClick = { expanded = true },
+            enabled = spinnerList.isNotEmpty(),
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 8.dp)
+        ) {
+            Text(
+                text = tempSelectedItem,
+                maxLines = 1,
+                fontWeight = FontWeight.SemiBold
+            )
+        }
+
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            spinnerList.forEach { item ->
+                DropdownMenuItem(
+                    onClick = {
+                        expanded = false
+                        onItemSelected(item)
+                        tempSelectedItem = item
+                    }
+                ) {
+                    Text(text = item)
+                }
             }
         }
     }
