839fdfb4801baca1de6946f2d828bfc2f423dd77
madhanmohans <madhanmohan.rms@gmail.com>
2023-03-15 15:02:19 +0530

    added supplier and sku spinners

--- app/src/main/java/com/example/fnvMockJetpack/ui/ProcurementScreen.kt
+++ app/src/main/java/com/example/fnvMockJetpack/ui/ProcurementScreen.kt
@@ -1,19 +1,21 @@
 package com.example.fnvMockJetpack.ui
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.fnvMockJetpack.components.SpinnerScreen
 import com.example.fnvMockJetpack.ui.ui.theme.FnvMockJetpackTheme
 
@@ -30,18 +32,33 @@ class ProcurementScreen : ComponentActivity() {
                     // TODO: setting the data in the ui, probably have to change
                     val supplierList = listOf("Supplier 1", "Supplier 2", "Supplier 3")
                     val skuList = listOf("SKU 1", "SKU 2", "SKU 3")
-                    Column() {
-                        var selectedSupplier by rememberSaveable() {
-                            mutableStateOf("")
-                        }
-                        SpinnerScreen(spinnerName = "Supplier", spinnerList = supplierList, selectedItem = selectedSupplier) {
+
+                    var selectedSupplier by rememberSaveable() { mutableStateOf("") }
+                    var selectedSKU by rememberSaveable() { mutableStateOf("") }
+
+                    Column(modifier = Modifier.padding(16.dp)) {
+                        SpinnerScreen(
+                            spinnerName = "Supplier",
+                            spinnerList = supplierList,
+                            selectedItem = selectedSupplier
+                        ) {
                             selectedSupplier = it
                         }
-                        var selectedSKU by rememberSaveable() {
-                            mutableStateOf("")
-                        }
-                        SpinnerScreen(spinnerName = "SKU", spinnerList = skuList, selectedItem = selectedSKU) {
+                        Spacer(modifier = Modifier.height(16.dp))
+                        SpinnerScreen(
+                            spinnerName = "SKU",
+                            spinnerList = skuList,
+                            selectedItem = selectedSKU
+                        ) {
                             selectedSKU = it
+                        }
+                        OutlinedButton(
+                            onClick = {  },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 20.dp, vertical = 24.dp)
+                        ) {
+                            Text(text = "Complete Activity")
                         }
                     }
                 }
@@ -50,6 +67,7 @@ class ProcurementScreen : ComponentActivity() {
     }
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Preview(showBackground = true, showSystemUi = false)
 @Composable
 fun DefaultPreview() {
@@ -58,20 +76,34 @@ fun DefaultPreview() {
         val supplierList = listOf("Supplier 1", "Supplier 2", "Supplier 3")
         val skuList = listOf("SKU 1", "SKU 2", "SKU 3")
 
-        Column() {
-            var selectedSupplier by rememberSaveable() {
-                mutableStateOf("")
-            }
-            SpinnerScreen(spinnerName = "Supplier", spinnerList = supplierList, selectedItem = selectedSupplier) {
+        var selectedSupplier by rememberSaveable() { mutableStateOf("") }
+        var selectedSKU by rememberSaveable() { mutableStateOf("") }
+
+        Column(modifier = Modifier.padding(16.dp)) {
+            SpinnerScreen(
+                spinnerName = "Supplier",
+                spinnerList = supplierList,
+                selectedItem = selectedSupplier
+            ) {
                 selectedSupplier = it
             }
-            var selectedSKU by rememberSaveable() {
-                mutableStateOf("")
-            }
-            SpinnerScreen(spinnerName = "SKU", spinnerList = skuList, selectedItem = selectedSKU) {
+            Spacer(modifier = Modifier.height(16.dp))
+            SpinnerScreen(
+                spinnerName = "SKU",
+                spinnerList = skuList,
+                selectedItem = selectedSKU
+            ) {
                 selectedSKU = it
             }
+            Button(
+                onClick = { },
+                colors = ButtonDefaults.buttonColors(Color.Black),
+                shape = RectangleShape,
+                modifier = Modifier
+                    .padding(16.dp)
+            ) {
+                Text(text = "Complete Activity")
+            }
         }
-
     }
 }
